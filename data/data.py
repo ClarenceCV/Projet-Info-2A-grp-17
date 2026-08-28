a45c64c9aa9ff032d9aaccbc41070712f7ee5d7a
clarencecv <clarence.chemlavidaud@gmail.com>
2026-08-28 14:11:33 +0000

    add: exemple d'utilisation de load_data

--- data/data.py
+++ data/data.py
@@ -20,3 +20,10 @@ def load_data(url):
     data = pd.read_csv(StringIO(reponse.text))
     return data
 
+# Exemple d'utilisation:
+# data_01 = load_data(url = "https://rplumber.ilo.org/data/indicator?id=EMP_5EMP_SEX_OC2_NB_Q&timefrom=2020&timeto=2026&type=label&format=.csv")
+# print(data_01)
+# ref_area.label               source.label  ...  note_indicator.label                                  note_source.label
+# 0              Angola    LFS - Employment Survey  ...  Frequency: Quarterly  Repository: ILO-STATISTICS - Micro data proces...
+# 1              Angola    LFS - Employment Survey  ...  Frequency: Quarterly  Repository: ILO-STATISTICS - Micro data proces...
+# ...
