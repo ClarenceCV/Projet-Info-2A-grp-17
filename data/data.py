5de680d333902bf5e3722ccb35fbd799c5659ca2
clarencecv <clarence.chemlavidaud@gmail.com>
2026-08-28 13:58:32 +0000

    add: fonction loader

--- data/data.py
+++ data/data.py
@@ -6,10 +6,9 @@ from io import StringIO
 
 url = "https://rplumber.ilo.org/data/indicator?id=EMP_5EMP_SEX_OC2_NB_Q&timefrom=2020&timeto=2026&type=label&format=.csv"
 
-reponse = requests.get(url)
-print(reponse) # si renvoie 200 Ok
-
-# Transformer la réponse CSV en DataFrame
-df_EMP_5EMP_SEX_OC2_NB_Q = pd.read_csv(StringIO(reponse.text))
-
-print(df_EMP_5EMP_SEX_OC2_NB_Q)
+def load_data(url):
+    reponse = requests.get(url)
+    if print(reponse) != 200:
+        raise ValueError("Il y a une erreur de connection avec le serveur")
+    data = pd.read_csv(StringIO(reponse.text))
+    return data
