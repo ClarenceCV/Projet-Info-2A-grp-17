ba8bc72362657b183d3c44dd6321f29897cdfd8e
clarencecv <clarence.chemlavidaud@gmail.com>
2026-09-04 14:22:01 +0000

    add: class profession + class indicator

---
+++ backend/src/buisness_object/indicateur.py
@@ -0,0 +1,12 @@
+class Indicator:
+    '''
+    Class object for indocators 
+    '''
+
+    def __init__(
+        self,
+        id_indicator: int
+    ):
+
+        self.id_indicator = id_indicator
+
--- backend/src/buisness_object/profession.py
+++ backend/src/buisness_object/profession.py
@@ -1,5 +1,20 @@
 class Profession:
     '''
-    Classe pour les professions
+    Profession object
+    Attributes:
+        id_profession (int): Unique identifier pro the profession.
+        libelle_profession (str): Name of the profession.
     '''
-    def __init__(self, id_profession, libelle_profession):
+    def __init__(
+        self,
+        id_profession: int,
+        libelle_profession: str
+    ):
+        """Constructor"""
+        if not isinstance(id_profession, int):
+            raise TypeError("Id should be an integer")
+        if not isinstance(libelle_profession, str):
+            raise TypeError("Libelle should be an chain of characters")
+
+        self.id_profession = id_profession
+        self.libelle_profession = libelle_profession
