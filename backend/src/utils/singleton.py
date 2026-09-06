4328d0f6e5f0436dc8186d37f0b9cdf760229e14
ethiquetlaurent <laurent.khaouli@laposte.net>
2026-09-06 17:59:44 +0200

    add: dao fichier de connexion à la base

---
+++ backend/src/utils/singleton.py
@@ -0,0 +1,16 @@
+# utile pour créer une instance unique dans le dao.db_connection
+# c.f. tp3
+
+class Singleton(type):
+    """
+    All classes that inherit from Singleton will have only one instance
+    https://refactoring.guru/fr/design-patterns/singleton
+    """
+
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
